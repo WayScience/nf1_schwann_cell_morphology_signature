3fbcfad3307ee359dd17ff6236ab7389d98c564f
jenna-tomkinson <jenna.tomkinson@ucdenver.edu>
2025-07-21 09:31:57 -0600

    update supp figure 5

--- 4.figures/supp_figure_5/scripts/supp_figure_5.r
+++ 4.figures/supp_figure_5/scripts/supp_figure_5.r
@@ -200,79 +200,6 @@ accuracy_score_plot <- (
 
 accuracy_score_plot
 
-# Load data
-kstest_results_file <- file.path("../../3.assess_generalizability/results/ks_test_derivatives_results_qc.parquet")
-
-kstest_results_df <- arrow::read_parquet(kstest_results_file)
-
-# Filter for genotype_comparison == "All"
-kstest_results_df <- kstest_results_df %>%
-    filter(genotype_comparison == "All")
-
-# Create a new column extracting the first part of 'feature' after the compartment
-kstest_results_df$feature_base <- sub("^[^_]+_", "", kstest_results_df$feature)
-
-# Update the channel column where anything other than DAPI, CY5, GFP, or RFP is called "other"
-kstest_results_df$channel <- ifelse(kstest_results_df$channel %in% c("DAPI", "CY5", "GFP", "RFP"), kstest_results_df$channel, "Other")
-
-# Update the channel names
-kstest_results_df$channel <- recode(kstest_results_df$channel, "DAPI" = "Nuclei", "GFP" = "ER", "CY5" = "Mito", "RFP" = "F-actin")
-
-# Arrange results by ks_stat in descending order
-kstest_results_df <- kstest_results_df %>%
-    arrange(desc(ks_stat))
-
-dim(kstest_results_df)
-head(kstest_results_df)
-
-width <- 10
-height <- 8
-options(repr.plot.width = width, repr.plot.height = height)
-
-# Update feature group name
-kstest_results_df$feature_group <- ifelse(kstest_results_df$feature_group == "RadialDistribution", "RadialDist", kstest_results_df$feature_group)
-
-# Reorder feature_base based on the sum of the base feature across all compartments
-kstest_results_df$feature_base <- factor(kstest_results_df$feature_base, 
-                                         levels = kstest_results_df %>% 
-                                           group_by(feature_base) %>% 
-                                           summarise(total_ks = sum(ks_stat)) %>% 
-                                           arrange(desc(total_ks)) %>% 
-                                           pull(feature_base))
-
-# Create the plot
-ks_test_scatter <- (
-    ggplot(kstest_results_df, aes(x = feature_base, y = ks_stat))
-    + geom_point(aes(color = feature_group, size = feature_importances, shape = channel), alpha = 0.4) 
-    + theme_bw()
-    + facet_grid(compartment ~ .)
-    + theme(
-        axis.text.x = element_blank(),
-        axis.text.y = element_text(size = 23),
-        axis.title.x = element_text(size = 20),
-        axis.title.y = element_text(size = 22),
-        legend.text = element_text(size = 20),
-        legend.title = element_text(size = 22),
-        strip.text = element_text(size = 22),
-        panel.spacing = unit(1, "lines"),
-        legend.position = "right"
-    )
-    + ylim(0,1)
-    + scale_color_discrete(name = "Feature\ngroup")
-    + scale_size_continuous(name = "Feature\nimportance", range = c(1, 8)) 
-    + scale_shape_manual(name = "Organelle", values = c(16, 17, 15, 18, 7))
-    + labs(
-        x = "Rank of summed KS statistic per core feature group\n(summed across compartments)",
-        y = "KS test statistic"
-    )
-    + guides(
-    shape = guide_legend(override.aes = list(size = 5)), 
-    color = guide_legend(override.aes = list(size = 5))
-    )
-)
-
-ks_test_scatter
-
 # Drop rows where genotype is 'HET'
 platemap_df <- platemap_df %>%
     filter(genotype != "HET")
@@ -328,17 +255,6 @@ right_plot <- (
 
 right_plot
 
-height <- 8
-width <- 15
-options(repr.plot.width = width, repr.plot.height = height)
-
-align_bottom_plot <- (
-    right_plot |
-    ks_test_scatter
-) + plot_layout(widths = c(1,1))
-
-align_bottom_plot
-
 height <- 10
 width <- 16
 options(repr.plot.width = width, repr.plot.height = height)
@@ -351,13 +267,13 @@ platemap_umap <- (
 platemap_umap
 
 height <- 12
-width <- 25
+width <- 16
 options(repr.plot.width = width, repr.plot.height = height)
 
 align_plot <- (
     platemap_umap |
-    align_bottom_plot
-) + plot_layout(widths = c(0.5,1))
+    right_plot
+) + plot_layout(widths = c(1,1))
 
 align_plot
 
